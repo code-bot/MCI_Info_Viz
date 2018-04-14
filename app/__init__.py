40f2efcfe51c72b36c0b04d4c04d745079594891
Sahaj Bhatt <sahajb97@gmail.com>
2018-04-14 19:01:03 -0400

    Created a function that attempts to find all device transmissions for a give date

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,26 @@
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-import database
+from modules import database
+from datetime import datetime
 
 app = Flask(__name__) # create the application instance
 app.config.from_object('config.Config') # load config
 
 @app.route('/')
-def home():
+def activity():
+
+	# # Sample SQL Server Query
+	# cursor = database.getCursor()
+	# rooms = cursor.execute("SELECT * FROM Rooms").fetchall();
+	# print(rooms)
+	# database.closeConnection()
+	#database.getAllDevicesAtDate(datetime.today())
+	print(database.getAllDevicesAtDate(datetime.today())[0])
+
+	return render_template('home.html')
+
+@app.route('/visualizations')
+def viz():
 
 	# Sample SQL Server Query
 	cursor = database.getCursor()
@@ -14,4 +28,4 @@ def home():
 	print(rooms)
 	database.closeConnection()
 
-	return "Home"
+	return "MY VIS"
---
+++ modules/database.py
@@ -0,0 +1,77 @@
+import pyodbc
+from config import Config
+
+_conn = None
+
+
+def getConnection():
+	global _conn
+	if _conn is None:
+		connectionString = 'DSN={};DATABASE={};UID={};PWD={}'.format(Config.DATABASE_HOST,Config.DATABASE_DB,Config.DATABASE_USER,Config.DATABASE_PASSWORD)
+		_conn = pyodbc.connect(connectionString)
+
+	return _conn
+
+def getCursor():
+	global _conn
+	try:
+		cursor = _conn.cursor()
+	except:
+		getConnection()
+		cursor = _conn.cursor()
+
+	return cursor
+
+def closeConnection():
+	global _conn
+	if _conn is not None:
+		try:
+			_conn.cursor.close()
+			_conn.close()
+			_conn = None
+		except:
+			_conn = None
+
+def getAllDevicesAtDate(date):
+	command = """SELECT DeviceID, DevicePlugNumber, WhatsPluggedIn, SensorID FROM DeviceSensors WHERE RecordStatus='A'"""
+	devices = getCursor().execute(command).fetchall()
+	deviceActivationList = []
+	for device in devices:
+		deviceID = device[0]
+		devicePlugNumber = device[1]
+		label = device[2]
+		transmissions = getDeviceAtDate(deviceID, date)
+		index = 0
+		startTime = [None]
+		endTime = [None]
+		time = None
+		for transmission in transmissions:
+			time = transmission[6]
+			if transmission[devicePlugNumber] > 0 and not startTime[index]:
+				startTime[index] = time
+				startTime.append(None)
+			elif transmission[devicePlugNumber] < 0 and not endTime[index]:
+				endTime[index] = time
+				endTime.append(None)
+				index += 1
+
+		if startTime[index] and not endTime[index]:
+			endTime[index] = time
+			index += 1
+
+		for i in range(index):
+			deviceActivationList.append([label, startTime[i], endTime[i]])
+
+	#print(deviceActivationList)
+	return deviceActivationList
+
+def getDeviceAtDate(deviceID, date):
+	date = date.strftime('%Y-%m-%d')
+	command = """SELECT DeviceID, WattsOutlet1, WattsOutlet2, WattsOutlet3, WattsOutlet4, WattsOutlet5, RxTimeStamp 
+				FROM DeviceTransmissions 
+				WHERE Convert(date, RxTimeStamp)=Convert(date, '{}') 
+				AND DeviceID={}
+				ORDER BY RxTimeStamp ASC""".format(date, deviceID)
+
+	transmissions = getCursor().execute(command).fetchall()
+	return transmissions
